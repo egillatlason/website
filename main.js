000dcffa3fe7e64a63a333d90579d507e75686e8
egillatlason <egillatlason95@gmail.com>
2020-02-18 16:34:58 +0000

    bætti við pagination

--- main.js
+++ main.js
@@ -1,51 +1,82 @@
-var list = new Array();
-var pageList = new Array();
-var currentPage = 1;
-var numberPerPage = 10;
-var numberOfPages = 1;
+    var list = new Array();
+    var pageList = new Array();
+    var currentPage = 1;
+    var numberPerPage = 10;
+    var numberOfPages = 0;
 
-function load(){
-    makeList();
+
+
+function makeList() {
+    var el = document.getElementsByClassName("update");
+    list = Array.from(el);
+    console.log("list ",list);
+
     numberOfPages = getNumberOfPages();
 }
-
+    
 function getNumberOfPages() {
     return Math.ceil(list.length / numberPerPage);
 }
 
 function nextPage() {
     currentPage += 1;
+    console.log("currentPage", currentPage);
     loadList();
+    topPage();
 }
 
 function previousPage() {
     currentPage -= 1;
     loadList();
+    topPage();
+
 }
 
 function firstPage() {
     currentPage = 1;
     loadList();
+    topPage();
 }
 
 function lastPage() {
     currentPage = numberOfPages;
     loadList();
+    topPage();
+
 }
 
 function loadList() {
     var begin = ((currentPage - 1) * numberPerPage);
     var end = begin + numberPerPage;
-
-    pageList = update.slice(begin, end);
+    pageList = list.slice(begin, end);
     drawList();
     check();
 }
-
-function drawList() {
-    document.getElementsByClassName("update").innerHTML = "";
     
+function drawList() {
+    document.getElementById("blog").innerHTML = "";
+    console.log(currentPage);
     for (r = 0; r < pageList.length; r++) {
-        document.getElementsByClassName("update").innerHTML += pageList[r] + "";
+        document.getElementById("blog").appendChild(pageList[r]);
     }
 }
+
+function check() {
+    document.getElementById("next").disabled = currentPage == numberOfPages ? true : false;
+    document.getElementById("previous").disabled = currentPage == 1 ? true : false;
+    document.getElementById("first").disabled = currentPage == 1 ? true : false;
+    document.getElementById("last").disabled = currentPage == numberOfPages ? true : false;
+}
+
+function load() {
+    makeList();
+    loadList();
+}
+
+function topPage() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+    
+window.onload = load;
